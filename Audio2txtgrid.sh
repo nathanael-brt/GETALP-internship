5a3b3972ff0dd2070db9c6510b0ad55c2058cc7d
nathanael-brt <nathanael.berthet@gmail.com>
2024-06-13 12:53:08 +0200

    new file:   Audio2txtgrid.sh 	new file:   Textgrid/dec2022-004_023-009.TextGrid 	new file:   Whisper/dec2022-004_023-009/dec2022-004_023-009.json 	new file:   Whisper/dec2022-004_023-009/dec2022-004_023-009.srt 	new file:   Whisper/dec2022-004_023-009/dec2022-004_023-009.tsv 	new file:   Whisper/dec2022-004_023-009/dec2022-004_023-009.txt 	new file:   Whisper/dec2022-004_023-009/dec2022-004_023-009.vtt 	new file:   srt/dec2022-004_023-009.srt

---
+++ Audio2txtgrid.sh
@@ -0,0 +1,23 @@
+#exectute whisper on an audio file and convert the result in Textgrid
+echo "--STARTING--"
+read
+#read the parameters
+echo Audio file path : 
+read file_path 
+echo Model : 
+read model
+#create the corresponding directory
+file_name=$(basename $file_path .wav)   #get the file name from the path
+cd Whisper
+mkdir $file_name
+cd $file_name
+#execute whisper
+whisper $file_path --model $model --language en
+#Textgrid format
+cp *.srt ../../srt/  #copy the .srt file in the correspondind directory
+cd ../../
+touch Textgrid/$file_name.TextGrid  #create the .TextGrid file
+chmod a+w Textgrid/$file_name.TextGrid
+#execute the python program
+python whisper2textgrid.py
+echo "--GOODBYE--"
